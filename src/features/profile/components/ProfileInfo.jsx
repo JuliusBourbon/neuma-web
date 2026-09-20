5d88d5f35f18e767a9e95f259da26891486f18ec
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-20 21:19:10 +0700

    refactor: use reusable field in profile info

--- src/features/profile/components/ProfileInfo.jsx
+++ src/features/profile/components/ProfileInfo.jsx
@@ -1,49 +1,18 @@
 import FillRoundedButton from "../../../components/common/fillRoundedButton";
+import ProfileField from "./ProfileField";
 
 function ProfileInfo({ profile, getGenderLabel, onEdit, onChangePassword }) {
   return (
     <>
       {/* Profile Information */}
       <div className="space-y-5 max-w-[650px]">
-        <div>
-          <label className="block text-xl mb-2">Name</label>
+        <ProfileField label="Name" value={profile.name} />
 
-          <input
-            value={profile.name}
-            disabled
-            className="w-full rounded-md bg-[#E5FE96] px-4 py-3 text-[#263200] outline-none"
-          />
-        </div>
+        <ProfileField label="Email" value={profile.email} />
 
-        <div>
-          <label className="block text-xl mb-2">Email</label>
+        <ProfileField label="Age" value={profile.age} />
 
-          <input
-            value={profile.email}
-            disabled
-            className="w-full rounded-md bg-[#E5FE96] px-4 py-3 text-[#263200] outline-none"
-          />
-        </div>
-
-        <div>
-          <label className="block text-xl mb-2">Age</label>
-
-          <input
-            value={profile.age}
-            disabled
-            className="w-full rounded-md bg-[#E5FE96] px-4 py-3 text-[#263200] outline-none"
-          />
-        </div>
-
-        <div>
-          <label className="block text-xl mb-2">Gender</label>
-
-          <input
-            value={getGenderLabel(profile.gender)}
-            disabled
-            className="w-full rounded-md bg-[#E5FE96] px-4 py-3 text-[#263200] outline-none"
-          />
-        </div>
+        <ProfileField label="Gender" value={getGenderLabel(profile.gender)} />
       </div>
 
       {/* Profile Buttons */}
