52581c85e5916647d0428614b76feea8dd6ce8c6
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-20 21:20:01 +0700

    refactor: update avatar card layout

--- src/features/profile/components/ProfileAvatarCard.jsx
+++ src/features/profile/components/ProfileAvatarCard.jsx
@@ -10,11 +10,11 @@ function ProfileAvatarCard({
   return (
     <div className="w-full max-w-[480px] bg-[#B6FF00] rounded-md overflow-hidden">
       {/* Avatar */}
-      <div className="h-[400px] border-8 border-[#B6FF00] flex items-center justify-center">
+      <div className="h-[280px] sm:h-[340px] md:h-[400px] border-8 border-[#B6FF00] flex items-center justify-center">
         <img
           src={avatar}
           alt="User avatar"
-          className="w-[220px] h-[220px] object-contain"
+          className="w-[150px] h-[150px] sm:w-[180px] sm:h-[180px] md:w-[220px] md:h-[220px] object-contain"
         />
       </div>
 
