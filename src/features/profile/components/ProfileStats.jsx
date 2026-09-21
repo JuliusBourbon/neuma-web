9fc40707b9926af082d82752c0086bdc52318c3e
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-21 20:49:59 +0700

    feat: update profile stats styling

--- src/features/profile/components/ProfileStats.jsx
+++ src/features/profile/components/ProfileStats.jsx
@@ -1,17 +1,46 @@
-function ProfileStats({ dayStreak, wordsCollected, totalXp, currencyBalance }) {
+import BookIcon from "../../../components/icons/bookIcon";
+import CoinIcon from "../../../components/icons/coinIcon";
+import RankIcon from "../../../components/icons/rankIcon";
+import StarIcon from "../../../components/icons/starIcon";
+import StreakIcon from "../../../components/icons/streakIcon";
+
+function ProfileStats({
+  dayStreak,
+  rank,
+  wordsCollected,
+  totalXp,
+  currencyBalance,
+}) {
   return (
     <div className="p-8 grid grid-cols-2 gap-8 text-[#263200]">
       {/* Day Streak */}
-      <div className="text-xl">🔥 {dayStreak} days</div>
+      <div className="text-xl flex items-center gap-2">
+        <StreakIcon color="#FE7236" />
+        {dayStreak} days
+      </div>
 
-      {/* Words Collected */}
-      <div className="text-xl">📖 {wordsCollected}</div>
+      {/* Rank */}
+      <div className="text-xl flex items-center gap-2">
+        <RankIcon color="#FE7236" />#{rank}
+      </div>
 
       {/* Total XP */}
-      <div className="text-xl">⭐ {totalXp} XP</div>
+      <div className="text-xl flex items-center gap-2">
+        <StarIcon color="#FE7236" />
+        {totalXp} XP
+      </div>
+
+      {/* Capaian Belajar */}
+      <div className="text-xl flex items-center gap-2">
+        <BookIcon color="#FE7236" />
+        {wordsCollected}
+      </div>
 
       {/* Currency */}
-      <div className="text-xl">🪙 {currencyBalance}</div>
+      <div className="text-xl flex items-center gap-2">
+        <CoinIcon color="#FE7236" />
+        {currencyBalance}
+      </div>
     </div>
   );
 }
